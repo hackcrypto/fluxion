4fd0460a79abb938af4c01ffce2363fd17a3ee78
hackcrypto <33383620+hackcrypto@users.noreply.github.com>
2018-06-30 10:06:35 +0000

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -60,8 +60,9 @@ function mostrarheader(){
 
 	echo -e "$blue#########################################################"
 	echo -e "$blue#                                                       #"
-  echo -e "$blue#  $red    FLUXION $version" "${yellow} ${red}  < F""${yellow}luxion" "${red}I""${yellow}s" "${red}T""${yellow}he ""${red}F""${yellow}uture >     "          ${blue}"     #"
+  echo -e "$blue#  $red    FLUXION $version" "${yellow} ${red}  < F""${yellow}luxion" "${red}I""${yellow}s" "${red}T""${yellow}he ""${red}F""${yellow}uture >     "          ${blue}"      #"
 	echo -e "$blue#"${yellow} by "${red}D""${yellow}eltax", "${red}"S""${yellow}"trasharo and "${red}A""${yellow}patheticEuphoria"           "    ${blue}#""
+	echo -e "$blue#"${yellow} remake by "${red}H""${yellow}ackcrypto                                  #"
 	echo -e "$blue#                                                       #"
 	echo -e "$blue#########################################################""$transparent"
 	echo
